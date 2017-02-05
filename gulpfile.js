9cf70fb51c7d913aad018fbcaeadedb50064c009
Halil İbrahim Şener <hisener@yahoo.com>
2017-02-05 14:04:32 +0300

    Add gulpfile for build

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const del = require('del')
+
+gulp.task('build', ['clean'], function () {
+  gulp.src(['projects/**', '!projects/personal-portfolio{,/**}'])
+    .pipe(gulp.dest('build'))
+  gulp.src(['projects/personal-portfolio/*'])
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('clean', function () {
+  return del('build/**', { force: true })
+})
+
+gulp.task('default', ['build'])
